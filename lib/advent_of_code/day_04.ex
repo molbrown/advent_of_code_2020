8a3d7575196c299d83e7071eb0683ab46d025cec
molbrown <molly@spreedly.com>
2020-12-04 13:51:55 -0500

    Day 4 part 1

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,14 @@
 defmodule AdventOfCode.Day04 do
-  def part1(args) do
+  @required_passport_data [ :byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid ]
+
+  def part1(input) do
+    input
+    |> Enum.filter(&valid?/1)
+    |> Enum.count()
+  end
+
+  def valid?(input) do
+    Enum.all?(@required_passport_data, fn field -> Keyword.has_key?(input, field) end)
   end
 
   def part2(args) do
--- lib/mix/tasks/d04.p1.ex
+++ lib/mix/tasks/d04.p1.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.D04.P1 do
 
   @shortdoc "Day 04 Part 1"
   def run(args) do
-    input = nil
+    input = get_input()
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
@@ -13,5 +13,29 @@ defmodule Mix.Tasks.D04.P1 do
         input
         |> part1()
         |> IO.inspect(label: "Part 1 Results")
+  end
+
+  def get_input do
+    "../../data/day_04.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Enum.map(&String.trim_trailing/1)
+    |> chunk_passports()
+    |> Enum.map(fn each -> Enum.map(each, fn subsection -> String.split(subsection, " ") end) end)
+    |> Enum.map(fn each -> Enum.concat(each) end)
+    |> Enum.map(fn each -> Enum.map(each, fn subsection -> Regex.split(~r{:}, subsection) end) end)
+    |> Enum.map(fn each -> Enum.map(each, fn [a, b] -> {String.to_atom(a), b} end) end)
+  end
+
+  def chunk_passports(input) do
+    one_passport = fn
+      (element, []) -> {:cont, [element]}
+      (element, chunk = [_ | _]) when element != "" -> {:cont, [element | chunk]}
+      (element, chunk = [_ | _]) when element == "" -> {:cont, chunk, []}
+    end
+
+    after_func = fn chunk -> {:cont, chunk, []} end
+
+    Enum.chunk_while(input, [], one_passport, after_func)
   end
 end
