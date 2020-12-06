5da2b1268f21e551c18c35b99e32dbd52e5f3c3c
molbrown <molly@spreedly.com>
2020-12-06 00:01:21 -0500

    Day 5 part 2

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -8,6 +8,20 @@ defmodule AdventOfCode.Day05 do
     |> Enum.max()
   end
 
+  def part2(input) do
+    input
+    |> Enum.map(&calc_seat_id/1)
+    |> Enum.sort()
+    |> find_missing_seat(0)
+  end
+
+  def find_missing_seat(input, index) do
+    cond do
+      Enum.at(input, index + 1) == Enum.at(input, index) + 1 -> find_missing_seat(input, index + 1)
+      true -> Enum.at(input, index) + 1
+    end
+  end
+
   def calc_seat_id(input) do
     calc_row(String.slice(input, 0..6), @rows) * 8 + calc_column(String.slice(input, 7..9), @columns)
   end
