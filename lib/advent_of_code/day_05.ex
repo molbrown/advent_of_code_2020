6fc9c9b243155bc287cf78525c229aed29c6199e
molbrown <molly@spreedly.com>
2020-12-05 23:54:53 -0500

    Day 5 part 1

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,38 @@
 defmodule AdventOfCode.Day05 do
-  def part1(args) do
+  @rows Enum.to_list(0..127) |> Enum.map(&Integer.to_string/1)
+  @columns Enum.to_list(0..7) |> Enum.map(&Integer.to_string/1)
+
+  def part1(input) do
+    input
+    |> Enum.map(&calc_seat_id/1)
+    |> Enum.max()
   end
 
-  def part2(args) do
+  def calc_seat_id(input) do
+    calc_row(String.slice(input, 0..6), @rows) * 8 + calc_column(String.slice(input, 7..9), @columns)
+  end
+
+  def calc_row(_ , range) when length(range) == 1 do
+    String.to_integer(Enum.at(range, 0))
+  end
+
+  def calc_row(input, range) do
+    {front, back} = Enum.split(range, Enum.count(range)/2 |> round())
+    cond do
+      String.at(input, 0) == "F" -> calc_row(String.slice(input, 1..6), front)
+      true -> calc_row(String.slice(input, 1..6), back)
+    end
+  end
+
+  def calc_column(_ , range) when length(range) == 1 do
+    String.to_integer(Enum.at(range, 0))
+  end
+
+  def calc_column(input, range) do
+    {left, right} = Enum.split(range, Enum.count(range)/2 |> round())
+    cond do
+      String.at(input, 0) == "L" -> calc_column(String.slice(input, 1..6), left)
+      true -> calc_column(String.slice(input, 1..6), right)
+    end
   end
 end
