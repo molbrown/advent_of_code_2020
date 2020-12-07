04b1d052967a0ba2debe05121ab16de3a11bc1f0
molbrown <molly@spreedly.com>
2020-12-06 22:35:08 -0500

    Day 6 part 2

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -3,11 +3,26 @@ defmodule AdventOfCode.Day06 do
     input
     |> Enum.map(fn group -> Enum.map(group, fn person -> String.graphemes(person) end) end)
     |> Enum.map(fn group -> Enum.reduce(group, [], fn person, acc -> acc ++ person end) end)
-    |> Enum.map( fn group -> Enum.uniq(group) end)
+    |> Enum.map(fn group -> Enum.uniq(group) end)
     |> Enum.map(fn group -> Enum.count(group) end)
     |> Enum.sum()
   end
 
-  def part2(args) do
+  def part2(input) do
+    input
+    |> Enum.map(fn group -> Enum.map(group, fn person -> String.graphemes(person) end) end)
+    |> Enum.map(&find_unions/1)
+    |> Enum.map(fn group -> Enum.count(group) end)
+    |> Enum.sum()
+  end
+
+  def find_unions(group) do
+    required_frequency = Enum.count(group)
+    first_person = Enum.at(group, 0)
+    group_result = Enum.reduce(group, [], fn person, acc -> acc ++ person end)
+
+    Enum.filter(first_person, fn question -> 
+      Enum.count(group_result, fn x -> x == question end) == required_frequency 
+    end)
   end
 end
--- lib/mix/tasks/d06.p2.ex
+++ lib/mix/tasks/d06.p2.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.D06.P2 do
 
   @shortdoc "Day 06 Part 2"
   def run(args) do
-    input = nil
+    input = get_input()
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
@@ -13,5 +13,25 @@ defmodule Mix.Tasks.D06.P2 do
         input
         |> part2()
         |> IO.inspect(label: "Part 2 Results")
+  end
+
+  def get_input() do
+    "../../data/day_06.txt"
+    |> Path.expand(__DIR__)
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
+    |> chunk_groups()
+  end
+
+  def chunk_groups(input) do
+    one_group = fn
+      (element, []) -> {:cont, [element]}
+      (element, chunk = [_ | _]) when element != "" -> {:cont, [element | chunk]}
+      (element, chunk = [_ | _]) when element == "" -> {:cont, chunk, []}
+    end
+
+    after_func = fn chunk -> {:cont, chunk, []} end
+
+    Enum.chunk_while(input, [], one_group, after_func)
   end
 end
